e64a790ad4bad8dcdae4b691c436a00b62fb41fc
Orlando Oliveira <ofertasdoparceiroorlando@gmail.com>
2025-11-22 22:43:10 -0300

    Ajuste de porta e host para Render

--- app.py
+++ app.py
@@ -40,5 +40,7 @@ def painel():
     return render_template('painel.html')
 
 # 🏁 Executa o app
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
